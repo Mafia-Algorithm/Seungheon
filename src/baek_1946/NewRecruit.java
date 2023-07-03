c94db286e3568956e5c8a7a2d5773e47b5c2e1f8
Lee SeungHeon <lsh328328@naver.com>
2023-07-03 22:57:11 +0900

    [1946] baekjoon with Java

--- src/baek_1946/NewRecruit.java
+++ src/baek_1946/NewRecruit.java
@@ -4,41 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class NewRecruit {
 
     static int T;
     static int N;
-    static int[][] score;
+    static int[][] ranking;
     static boolean[] isPass;
     static int result = 0;
 
-    static void passCheck(int[][] nowScore) {
-        for (int i = 1; i < nowScore.length - 1; i++) {
+    static void passCheck(int[][] ranking) {
 
-            // 이미 탈락 처리가 된 응시자는 확인하지 않음
-            if (!isPass[i]) {
-                continue;
-            }
+        // 서류 1등의 면접 점수가 처음 기준
+        int interviewPassRank = ranking[0][1];
 
-            for (int j = i + 1; j < nowScore.length; j++) {
-                System.out.println(nowScore[i][0] + " / " + nowScore[i][1]);
-                System.out.println(nowScore[j][0] + " / " + nowScore[j][1]);
+        // 서류 1등은 합격
+        isPass[0] = true;
 
-                // 본인이 다른 지원자에 비해 두 분야 모두 뛰어나다면
-                if (nowScore[i][0] > nowScore[j][0] && nowScore[i][1] > nowScore[j][1]) {
-                    System.out.println(i);
-                    System.out.println(j);
-                    isPass[j] = false;
-                }
-
-                // 본인이 다른 지원자에 비해 두 분야 모두 떨어진다면
-                if (nowScore[i][0] < nowScore[j][0] && nowScore[i][1] < nowScore[j][1]) {
-                    System.out.println(i);
-                    System.out.println(j);
-                    isPass[i] = false;
-                    break;
-                }
+        for (int i = 1; i <= ranking.length - 1; i++) {
+            // 면접 등수가 현재 기준보다 더 높다면
+            if (ranking[i][1] < interviewPassRank) {
+                isPass[i] = true;
+                interviewPassRank = ranking[i][1];
             }
         }
     }
@@ -50,14 +38,21 @@ public class NewRecruit {
 
         for (int test = 0; test < T; test++) {
             N = Integer.parseInt(br.readLine());
-            isPass = new boolean[N + 1];
-            Arrays.fill(isPass, true);
+            isPass = new boolean[N];
 
-            score = new int[N + 1][2];
-            for (int applicant = 1; applicant <= N; applicant++) {
-                score[applicant] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            ranking = new int[N][2];
+            for (int applicant = 0; applicant < N; applicant++) {
+                ranking[applicant] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
             }
-            passCheck(score);
+
+            // 서류 성적 기준으로 내림차순 정리
+            Arrays.sort(ranking, new Comparator<int[]>() {
+                @Override
+                public int compare(int[] o1, int[] o2) {
+                    return o1[0] - o2[0];
+                }
+            });
+            passCheck(ranking);
 
             for (boolean pass : isPass) {
                 if (pass) {
@@ -65,8 +60,7 @@ public class NewRecruit {
                 }
             }
 
-            System.out.println(Arrays.toString(isPass));
-            System.out.println(result - 1);
+            System.out.println(result);
             result = 0;
         }
     }
