751b4352285b95609b46dadbb9d67e94c601c063
Lee SeungHeon <lsh328328@naver.com>
2023-07-09 23:07:05 +0900

    [20529]: baekjoon with Java

--- src/baek_20529/MBTI.java
+++ src/baek_20529/MBTI.java
@@ -3,14 +3,24 @@ package baek_20529;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class MBTI {
 
     static int T;
     static int N;
-    static String[] mbtiList;
-    static int[] mbtiCount;
-    static int result = Integer.MAX_VALUE;
+    static String[] mbtiList = new String[]{"ISFP", "ISFJ", "ISTP", "ISTJ", "INFP", "INFJ", "INTP", "INTJ", "ESFP", "ESFJ", "ESTP", "ESTJ", "ENFP", "ENFJ", "ENTP", "ENTJ"};
+    static String[] mbtiInput;
+    static String[] temp;
+
+    static void recursion(int index, int depth) {
+        if (index == 3) {
+            return;
+        }
+
+        temp[depth] = mbtiInput[index];
+        recursion(index + 1, depth + 1);
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,62 +29,12 @@ public class MBTI {
 
         for (int i = 0; i < T; i++) {
             N = Integer.parseInt(br.readLine());
-            mbtiList = br.readLine().split(" ");
+            mbtiInput = br.readLine().split(" ");
 
-            for (int k = 0; k < N - 2; k++) {
-                for (int l = k + 1; l < N - 1; l++) {
-                    for (int m = l + 1; m < N; m++) {
-                        String[] temp = {mbtiList[k], mbtiList[l], mbtiList[m]};
-                        mbtiCount = new int[8];
+            for (int j = 0; j <= N -3; j++) {
+                recursion(j, 0);
 
-                        for (String mbti : temp) {
-                            for (int n = 0; n < 4; n++) {
-                                char alphabet = mbti.charAt(n);
-
-                                if (alphabet == 'E') {
-                                    mbtiCount[0]++;
-                                }
-
-                                if (alphabet == 'I') {
-                                    mbtiCount[1]++;
-                                }
-
-                                if (alphabet == 'N') {
-                                    mbtiCount[2]++;
-                                }
-
-                                if (alphabet == 'S') {
-                                    mbtiCount[3]++;
-                                }
-
-                                if (alphabet == 'T') {
-                                    mbtiCount[4]++;
-                                }
-
-                                if (alphabet == 'F') {
-                                    mbtiCount[5]++;
-                                }
-
-                                if (alphabet == 'J') {
-                                    mbtiCount[6]++;
-                                }
-
-                                if (alphabet == 'P') {
-                                    mbtiCount[7]++;
-                                }
-                            }
-                        }
-
-                        int distance = mbtiCount[0] * mbtiCount[1] + mbtiCount[2] * mbtiCount[3] + mbtiCount[4] * mbtiCount[5] + mbtiCount[6] * mbtiCount[7];
-                        if (distance < result) {
-                            result = distance;
-                        }
-                    }
-                }
             }
-
-            System.out.println(result);
-            result = Integer.MAX_VALUE;
         }
     }
 }
