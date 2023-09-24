05be8a97a056b738f9fed57ed128f942b21d608a
dev-Crayon <seungheon328@gmail.com>
2023-09-24 10:30:04 +0900

    [Programmers]: Hamburger with Java

--- src/pro_hamburger/Hamburger.java
+++ src/pro_hamburger/Hamburger.java
@@ -31,7 +31,7 @@ public class Hamburger {
 
     public static void main(String[] args) {
         System.out.println(solution(
-                new int[]{2, 1, 1, 2, 3, 1, 2, 3, 1}
+                new int[]{1, 3, 2, 1, 2, 1, 3, 1, 2}
         ));
     }
 }
