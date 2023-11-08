7de3bf8cca9fb74de295f12af0bd5c43afe6bd6b
dev-Crayon <seungheon328@gmail.com>
2023-11-08 15:40:38 +0900

    [Programmers String]: babbling with Java

---
+++ src/pro_string/Application.java
@@ -0,0 +1,8 @@
+package pro_string;
+
+public class Application {
+    public static void main(String[] args) {
+        Babbling1 babbling1 = new Babbling1();
+        System.out.println(babbling1.solution(new String[]{"aya", "yee", "u", "maa", "wyeoo"}));
+    }
+}
