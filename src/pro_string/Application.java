22cbf15bca3e28a24f2e7ab317b0fd4b93a608c3
dev-Crayon <seungheon328@gmail.com>
2023-11-10 22:41:31 +0900

    [Programmers String]: HideNumber with Java

--- src/pro_string/Application.java
+++ src/pro_string/Application.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        OXQuiz oxQuiz = new OXQuiz();
-        System.out.println(Arrays.toString(oxQuiz.solution(new String[]{"19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"})));
+        HideNumber hideNumber = new HideNumber();
+        System.out.println(hideNumber.solution("aAb1B2cC34oOp"));
     }
 }
