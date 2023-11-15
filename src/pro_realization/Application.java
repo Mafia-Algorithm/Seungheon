10c04ce3b71ab1f70498914ad3bc11b560c7524b
dev-Crayon <seungheon328@gmail.com>
2023-11-15 16:25:18 +0900

    [Programmers Realization]: ChickenCoupon with Java

--- src/pro_realization/Application.java
+++ src/pro_realization/Application.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        ContinuousNumber continuousNumber = new ContinuousNumber();
-        System.out.println(Arrays.toString(continuousNumber.solution(5, 15)));
+        ChickenCoupon chickenCoupon = new ChickenCoupon();
+        System.out.println(chickenCoupon.solution(1081));
     }
 }
