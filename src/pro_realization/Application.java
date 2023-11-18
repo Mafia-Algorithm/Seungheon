cae421d02ed9d05866b0bbf8c88e388bf6b52d7d
dev-Crayon <seungheon328@gmail.com>
2023-11-18 11:06:48 +0900

    [Programmers Realization]: MakeTwoDemension with Java

--- src/pro_realization/Application.java
+++ src/pro_realization/Application.java
@@ -1,7 +1,15 @@
 package pro_realization;
 
+import pro_array.MakeTwoDimension;
+
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        MakeTwoDimension makeTwoDimension = new MakeTwoDimension();
+        System.out.println(Arrays.deepToString(makeTwoDimension.solution(
+                new int[]{1, 2, 3, 4, 5, 6, 7, 8},
+                2
+        )));
     }
 }
