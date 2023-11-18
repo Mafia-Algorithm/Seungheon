db9b8f10fca7af8d9a7d5e86e00bff06508a6be5
dev-Crayon <seungheon328@gmail.com>
2023-11-18 11:12:05 +0900

    [Programmers Realization]: SevenCount with Java

--- src/pro_array/Application.java
+++ src/pro_array/Application.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Application {
     public static void main(String[] args) {
-        SplitArray splitArray = new SplitArray();
-        System.out.println(Arrays.toString(splitArray.solution("123123123", 3)));
+        SevenCount sevenCount = new SevenCount();
+        System.out.println(sevenCount.solution(new int[]{7, 77, 17}));
     }
 }
