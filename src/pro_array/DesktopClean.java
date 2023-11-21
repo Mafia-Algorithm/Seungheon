ba51b358dc7f2ed9bab454bb9ee739a127ffe238
dev-Crayon <seungheon328@gmail.com>
2023-11-21 09:50:20 +0900

    [Programmers Array]: DesktopClean with Java

---
+++ src/pro_array/DesktopClean.java
@@ -0,0 +1,33 @@
+package pro_array;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class DesktopClean {
+    public int[] solution(String[] wallpaper) {
+        ArrayList<Integer> xPoint = new ArrayList<>();
+        ArrayList<Integer> yPoint = new ArrayList<>();
+
+        String[][] desktop = new String[wallpaper.length][wallpaper[0].length()];
+        for (int i = 0; i < wallpaper.length; i++) {
+            desktop[i] = wallpaper[i].split("");
+        }
+
+        for (int i = 0; i < wallpaper.length; i++) {
+            for (int j = 0; j < wallpaper[0].length(); j++) {
+                // 파일인 경우
+                if (desktop[i][j].equals("#")) {
+                    xPoint.add(j);
+                    yPoint.add(i);
+                }
+            }
+        }
+
+        int minX = Collections.min(xPoint);
+        int minY = Collections.min(yPoint);
+        int maxX = Collections.max(xPoint);
+        int maxY = Collections.max(yPoint);
+
+        return new int[]{minY, minX, maxY + 1, maxX + 1};
+    }
+}
