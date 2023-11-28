049db7f3578ac424650841d3f37561451ad181af
dev-Crayon <seungheon328@gmail.com>
2023-11-28 11:44:31 +0900

    [Programmers Set]: MostFrequentValue with Java

--- src/pro_set/Application.java
+++ src/pro_set/Application.java
@@ -2,7 +2,7 @@ package pro_set;
 
 public class Application {
     public static void main(String[] args) {
-        DuplicationDelete duplicationDelete = new DuplicationDelete();
-        System.out.println(duplicationDelete.solution("We are the world"));
+        MostFrequentValue mostFrequentValue = new MostFrequentValue();
+        System.out.println(mostFrequentValue.solution(new int[]{1, 1, 2, 2}));
     }
 }
