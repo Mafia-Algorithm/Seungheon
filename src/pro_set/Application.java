a30de05e4c17b7538a5416d0ea09c7991b9ead3d
dev-Crayon <seungheon328@gmail.com>
2023-11-28 13:15:27 +0900

    [Programmers Set]: BothSecret with Java

--- src/pro_set/Application.java
+++ src/pro_set/Application.java
@@ -2,7 +2,7 @@ package pro_set;
 
 public class Application {
     public static void main(String[] args) {
-        Phonekemon phonekemon = new Phonekemon();
-        System.out.println(phonekemon.solution(new int[]{3,3,3,2,2,2}));
+        BothSecret bothSecret = new BothSecret();
+        System.out.println(bothSecret.solution("aukks", "wbqd", 5));
     }
 }
