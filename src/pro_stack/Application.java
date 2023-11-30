1cfb7cb85b003a045d1387d9aea96e6af2858c63
dev-Crayon <seungheon328@gmail.com>
2023-11-30 11:00:13 +0900

    [Programmers Stack]: ControlZ with Java

---
+++ src/pro_stack/Application.java
@@ -0,0 +1,8 @@
+package pro_stack;
+
+public class Application {
+    public static void main(String[] args) {
+        ControlZ controlZ = new ControlZ();
+        System.out.println(controlZ.solution("1 2 Z 3"));
+    }
+}
