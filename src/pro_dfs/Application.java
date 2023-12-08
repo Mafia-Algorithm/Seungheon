4e2192b17adc1436caadfcca2c91938492fcb14e
dev-Crayon <seungheon328@gmail.com>
2023-12-08 16:42:52 +0900

    [Programmers dfs]: Collatz with Java

---
+++ src/pro_dfs/Application.java
@@ -0,0 +1,8 @@
+package pro_dfs;
+
+public class Application {
+    public static void main(String[] args) {
+        Collatz collatz = new Collatz();
+        System.out.println(collatz.solution(626331));
+    }
+}
