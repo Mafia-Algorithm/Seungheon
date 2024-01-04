fbaf114697e370126f6184c5133aba2981a2a97c
dev-Crayon <seungheon328@gmail.com>
2024-01-04 11:56:52 +0900

    [Programmers realization]: KnightWeapon with Java

--- src/pro_realization/Application.java
+++ src/pro_realization/Application.java
@@ -1,13 +1,8 @@
 package pro_realization;
 
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
-       ParkWalk parkWalk = new ParkWalk();
-        System.out.println(Arrays.toString(parkWalk.solution(
-                new String[]{"OSO","OOO","OXO","OOO"},
-                new String[]{"E 2","S 3","W 1"}
-        )));
+        KnightWeapon knightWeapon = new KnightWeapon();
+        System.out.println(knightWeapon.solution(10, 3, 2));
     }
 }
