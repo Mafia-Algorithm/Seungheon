28e5596d301569b333a45945afc3200386baabd7
dev-Crayon <seungheon328@gmail.com>
2024-01-04 10:02:13 +0900

    [Programmers realization]: SafetyZone with Java

--- src/pro_realization/Application.java
+++ src/pro_realization/Application.java
@@ -1,15 +1,16 @@
 package pro_realization;
 
-import pro_array.MakeTwoDimension;
-
-import java.util.Arrays;
-
 public class Application {
     public static void main(String[] args) {
-        MakeTwoDimension makeTwoDimension = new MakeTwoDimension();
-        System.out.println(Arrays.deepToString(makeTwoDimension.solution(
-                new int[]{1, 2, 3, 4, 5, 6, 7, 8},
-                2
-        )));
+       SafetyZone safetyZone = new SafetyZone();
+        System.out.println(safetyZone.solution(
+                new int[][] {
+                        {1, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0},
+                        {0, 0, 0, 0, 0}
+                }
+        ));
     }
 }
