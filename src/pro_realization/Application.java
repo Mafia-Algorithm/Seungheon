ce2f369cb20d7a0169d84a3ead2cd0155d8b7860
dev-Crayon <seungheon328@gmail.com>
2024-01-04 11:18:09 +0900

    [Programmers realization]: ParkWalk with Java

--- src/pro_realization/Application.java
+++ src/pro_realization/Application.java
@@ -1,12 +1,13 @@
 package pro_realization;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
-       PaintOver paintOver = new PaintOver();
-       System.out.println(paintOver.solution(
-               8,
-               4,
-               new int[]{2, 3, 6}
-       ));
+       ParkWalk parkWalk = new ParkWalk();
+        System.out.println(Arrays.toString(parkWalk.solution(
+                new String[]{"OSO","OOO","OXO","OOO"},
+                new String[]{"E 2","S 3","W 1"}
+        )));
     }
 }
