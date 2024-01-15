0d566af999c0265a91b11e287256380f9e75fcdb
dev-Crayon <seungheon328@gmail.com>
2024-01-15 09:24:20 +0900

    [Baekjoon silver1]: OperationInput with Java

--- src/baek_silver1/EasyStairs.java
+++ src/baek_silver1/EasyStairs.java
@@ -10,42 +10,42 @@ public class EasyStairs {
     static long[][] dp;
     static int N;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        dp = new long[N + 1][10];
-
-        if (N == 1) {
-            System.out.println(9);
-            return;
-        }
-
-        if (N == 2) {
-            System.out.println(17);
-            return;
-        }
-
-        for (int i = 0; i <= 9; i++) {
-            dp[1][i] = 1L;
-        }
-
-        for (int i = 2; i <= N; i++) {
-            dp[i][0] = dp[i - 1][1];
-            for (int j = 1; j <= 9; j++) {
-                if (j == 9) {
-                    dp[i][j] = dp[i - 1][8] % MOD;
-                } else {
-                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
-                }
-            }
-        }
-
-        long result = 0;
-        for (int i = 1; i <= 9; i++) {
-            result = (result + dp[N][i]) % MOD;
-        }
-
-        System.out.println(result);
-    }
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//        N = Integer.parseInt(br.readLine());
+//        dp = new long[N + 1][10];
+//
+//        if (N == 1) {
+//            System.out.println(9);
+//            return;
+//        }
+//
+//        if (N == 2) {
+//            System.out.println(17);
+//            return;
+//        }
+//
+//        for (int i = 0; i <= 9; i++) {
+//            dp[1][i] = 1L;
+//        }
+//
+//        for (int i = 2; i <= N; i++) {
+//            dp[i][0] = dp[i - 1][1];
+//            for (int j = 1; j <= 9; j++) {
+//                if (j == 9) {
+//                    dp[i][j] = dp[i - 1][8] % MOD;
+//                } else {
+//                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+//                }
+//            }
+//        }
+//
+//        long result = 0;
+//        for (int i = 1; i <= 9; i++) {
+//            result = (result + dp[N][i]) % MOD;
+//        }
+//
+//        System.out.println(result);
+//    }
 }
