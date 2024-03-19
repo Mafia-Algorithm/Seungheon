dc98d9942b0c4f4776cc853406eedd4c0a37003f
dev-Crayon <seungheon328@gmail.com>
2024-03-19 17:03:35 +0900

    [Baekjoon silver1]: Palindrome with Java

--- src/baek_silver1/Palindrome.java
+++ src/baek_silver1/Palindrome.java
@@ -7,7 +7,7 @@ import java.io.InputStreamReader;
 public class Palindrome {
     static int n;
 
-    public static void main(String args[]) throws IOException {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         if (n == 1) {
