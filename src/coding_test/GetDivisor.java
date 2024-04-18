adc7a9e1a25a18061ac17c1b34f791abbccd6ff9
dev-Crayon <seungheon328@gmail.com>
2024-04-18 13:51:56 +0900

    [Baekjoon coding test]: GetDivisor with Java

--- src/coding_test/GetDivisor.java
+++ src/coding_test/GetDivisor.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-public class GetDivisior {
+public class GetDivisor {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
