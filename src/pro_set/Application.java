5d54e023c1f4370d6aee222c4b220f2656ab106e
dev-Crayon <seungheon328@gmail.com>
2024-09-02 16:34:04 +0900

    [Clear]

--- src/pro_set/Application.java
+++
@@ -1,8 +0,0 @@
-package pro_set;
-
-public class Application {
-    public static void main(String[] args) {
-        BothSecret bothSecret = new BothSecret();
-        System.out.println(bothSecret.solution("aukks", "wbqd", 5));
-    }
-}
--- src/pro_stack/Application.java
+++
@@ -1,8 +0,0 @@
-package pro_stack;
-
-public class Application {
-    public static void main(String[] args) {
-        CorrectBracket correctBracket = new CorrectBracket();
-        System.out.println(correctBracket.solution(")()("));
-    }
-}
